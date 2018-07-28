d522efb933dac662369d6646de8927473038049a
Jared Forsyth <jared@jaredforsyth.com>
2018-07-27 22:10:43 -0600

    fix syntax error

--- client/src/index.js
+++ client/src/index.js
@@ -103,12 +103,11 @@ function activate(context) {
                 command: binLocation,
                 args: [],
             },
-            {
-                ...clientOptions,
+            Object.assign({}, clientOptions, {
                 revealOutputChannelOn: vscode.workspace.getConfiguration('reason_language_server').get('show_debug_errors')
                     ? RevealOutputChannelOn.Error
                     : RevealOutputChannelOn.Never
-            }
+            }),
         );
         lastStartTime = Date.now()
         context.subscriptions.push(client.start());
