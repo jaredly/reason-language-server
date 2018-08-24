d7e7310201fe5a2127b6bfbeba4e503d42203949
Jared Forsyth <jared@jaredforsyth.com>
2018-08-24 09:57:39 -0600

    1.0.3 - fix windows, better completion

--- release.sh
+++ release.sh
@@ -5,6 +5,10 @@ rm -rf _zip
 mkdir -p _zip/reason-language-server
 cd _zip
 
+chmod +x ../editor-extensions/vscode/bin.native
+chmod +x ../editor-extensions/vscode/bin.native.exe
+chmod +x ../editor-extensions/vscode/bin.native.linux
+
 cp ../editor-extensions/vscode/bin.native reason-language-server/reason-language-server.exe
 chmod +x reason-language-server/reason-language-server.exe
 zip -r ../editor-extensions/vscode/macos.zip reason-language-server
