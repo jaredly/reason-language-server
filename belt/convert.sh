b8a387dac1860d5a8a4bf688552dc0a86a13ce24
Jared Forsyth <jared@jaredforsyth.com>
2018-09-06 23:36:01 -0600

    move to src

--- belt/convert.sh
+++ belt/convert.sh
@@ -1,7 +1,7 @@
 set -e
-for i in *.ml; do
-../bsb_modules/bs-platform/lib/bsrefmt $i --print binary > $i.bin
+for i in src/*.ml; do
+COMPILE_TO_NATIVE=true ../bsb_modules/bs-platform/lib/bspp.exe $i | ../bsb_modules/bs-platform/lib/bsrefmt --print binary > $i.bin
 COMPILE_TO_NATIVE=1 ../bsb_modules/bs-platform/lib/belt_bsppx.exe $i.bin $i.bin2
-../bsb_modules/bs-platform/lib/bsrefmt --parse binary $i.bin2 --print ml > $i
+../bsb_modules/bs-platform/lib/bsrefmt --parse binary $i.bin2 --print ml > $(basename $i)
 rm $i.bin $i.bin2
 done
