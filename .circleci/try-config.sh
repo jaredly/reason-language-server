1b64b7009327284ca0427eb9863574d403a4c9af
Jared Forsyth <jared@jaredforsyth.com>
2018-09-08 00:56:23 -0600

    maybe upload things

--- .circleci/try-config.sh
+++ .circleci/try-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user `cat ~/.ssh/circle-token`: \
     --request POST \
-    --form revision=5a18cb68ae32de30e0504d7b8398fc8fa088092a\
+    --form revision=c1bd97a7e136cd54f6ec00048776f5badaa361a6\
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/jaredly/reason-language-server/tree/master
