129ed1515dd413a7e47474fa39cef54ee0a46a11
Jared Forsyth <jared@jaredforsyth.com>
2019-12-17 13:37:49 -0700

    rename

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@ esy
 TARGET_BIN=$(esy echo '#{self.target_dir}/install/default/bin')
 $TARGET_BIN/UtilTests.exe
 $TARGET_BIN/ExamplesTests
-$TARGET_BIN/AllTests
+$TARGET_BIN/AnalyzeFixtureTests
 BASE=old_ocamls/407
 (cd $BASE; esy)
-$TARGET_BIN/AllTests -b $BASE
+$TARGET_BIN/AnalyzeFixtureTests -b $BASE
